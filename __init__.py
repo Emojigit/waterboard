1405c8cdde21c02cae131bcde5faefea1ea8fa11
Emoji <55009343+Emojigit@users.noreply.github.com>
2022-05-25 19:34:22 +0800

    check other water

--- __init__.py
+++ __init__.py
@@ -78,6 +78,7 @@ def setup(bot,storage):
                     place += 1
             returns.append("運行指令 /waterboard 獲取最新水群資訊！")
             returns.append("運行指令 /selfwater 獲取自己的水群資訊！")
+            returns.append("運行指令 /water [提及或用戶ID] 獲取他人的水群資訊（也可回覆別人省略參數）！")
             await event.respond("\n".join(returns),silent=True)
         raise events.StopPropagation
     @bot.on(events.NewMessage(pattern="/selfwater"))
@@ -102,6 +103,55 @@ def setup(bot,storage):
                     await event.respond("[你](tg://user?id={})水了 {} 條信息，群內第 {} 名，{}。".format(sender.id,waters_sorted[n][1],n + 1,band))
                     raise events.StopPropagation
             await event.respond("[你](tg://user?id={})沒有水過任何信息。".format(sender.id))
+            raise events.StopPropagation
+    @bot.on(events.NewMessage(pattern="/water"))
+    async def water(event):
+        if event.is_private:
+            await event.respond("此指令只在群組有效。")
+            raise events.StopPropagation
+        async with bot.action(event.chat, 'typing') as action:
+            chatid = event.chat_id
+            text = event.message.text
+            sender = event.sender
+            try:
+                text = text.split(" ",1)[1]
+            except IndexError:
+                text = ""
+            if text.replace(" ","") != "":
+                try:
+                    text = int(text)
+                except ValueError:
+                    if text[0] == "@":
+                        text = text[1:]
+                try:
+                    target = await bot.get_input_entity(text)
+                except (ValueError,TypeError):
+                    await event.respond("找不到用戶。格式：/water [提及或用戶ID，也可回覆代替]")
+                    raise events.StopPropagation
+            else:
+                rep = await event.get_reply_message()
+                if rep != None:
+                    target = rep.sender
+                else:
+                    await event.respond("找不到用戶。格式：/water [提及或用戶ID，也可回覆代替]")
+                    raise events.StopPropagation
+            storage_key = "waters_{}".format(chatid)
+            waters = storage.get(storage_key,{})
+            waters_sorted = sorted(waters.items(), key = lambda x: int(x[1]) * -1)
+            try:
+                id = target.user_id
+            except AttributeError:
+                id = target.id
+            for n in range(0,len(waters_sorted)):
+                if int(waters_sorted[n][0]) == id:
+                    band = get_bandage(waters_sorted[n][1],short=False)
+                    if band == None:
+                        band = "無勳章"
+                    else:
+                        band = "勳章爲" + band
+                    await event.respond("[他](tg://user?id={})水了 {} 條信息，群內第 {} 名，{}。".format(id,waters_sorted[n][1],n + 1,band))
+                    raise events.StopPropagation
+            await event.respond("[他](tg://user?id={})沒有水過任何信息。".format(id))
             raise events.StopPropagation
 
 
